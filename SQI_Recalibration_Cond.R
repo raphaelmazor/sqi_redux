4d634cbc0292e3e3ea89c497c66abdfbe7f92576
Heili Lowman <heilil@sccwrp.org>
2020-09-14 15:53:44 -0700

    Added script for conductivity data exploration.

---
+++ SQI_Recalibration_Cond.R
@@ -0,0 +1,13 @@
+# SQI Recalibration Script re: Conductivity
+# Creator: Heili Lowman
+# Date: September 14th
+# contributor: Jhen Cabasal
+
+#### Packages ####
+
+# Attach necessary packages.
+library(tidyverse)
+
+#### Data ####
+
+# Load in necessary SMC datasets.
