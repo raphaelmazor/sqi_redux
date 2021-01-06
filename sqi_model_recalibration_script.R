fb6967f479dd4ccb0107cb58a9316a242e92f59b
Heili Lowman <heilil@sccwrp.org>
2021-01-05 16:19:37 -0800

    New dataset creation and model recalibration scripts currently depend on one another - will be fixed tomorrow.

--- sqi_model_recalibration_script.R
+++ sqi_model_recalibration_script.R
@@ -18,60 +18,47 @@ sqidat <- sqidat %>%
 
 # get calibration/validation datasets
 set.seed(500)
+
 mydf.t<- sqidat %>% group_by(bio_fp)
-my.sites <- unique(mydf.t[, c('comid', 'bio_fp')])
-# Marcus did by MasterID but I'm switching to comid
+# want both passing and failing sites in calibration and validation datasets
+
+my.sites <- unique(mydf.t[, c('masterid', 'bio_fp')])
+
 sites.cal <- sample_frac(my.sites, 0.75, replace = F) %>% 
   group_by('bio_fp')
+
 mydf.t <- mydf.t %>% 
-  mutate(SiteSet = ifelse(comid %in% sites.cal$comid, 'Cal', 'Val')) %>% 
-  select(comid, yr, SiteSet)
+  mutate(SiteSet = ifelse(masterid %in% sites.cal$masterid, 'Cal', 'Val')) %>% 
+  select(masterid, yr, SiteSet)
+
 sqidat <- sqidat %>% 
-  left_join(mydf.t, by = c('comid', 'yr', 'bio_fp'))
+  left_join(mydf.t, by = c('masterid', 'yr', 'bio_fp'))
 
 # separate cal, val data
 caldat <- sqidat %>% 
   filter(SiteSet %in% 'Cal')
+
 valdat <- sqidat %>% 
   filter(SiteSet %in% 'Val')
 
 # models, glm
+# the following models use the same variables as those in Beck et al., 2019
+# bio_fp is pass/fail
+# 'logit' makes it logistic regression
+
 # pChem
 wqglm <- glm(bio_fp ~ log10(0.1 + tn) + log10(0.01 + tp) + Cond,
   family = binomial('logit'), data = caldat)
-wqglm <- step(wqglm)
 
 # pHab
-vif_func(caldat[, c('bs', 'hy', 'ps', 'ev_flowhab', 'h_aqhab', 'h_subnat', 'pct_safn', 'xcmg')], thresh = 3)
-habglm <- glm(bio_fp ~ hy + ps + ev_flowhab + h_aqhab + pct_safn + xcmg, 
+habglm <- glm(bio_fp ~ hy + pct_safn + xcmg, 
   family = binomial('logit'), data = caldat)
-habglm <- step(habglm)
-
-# save to package 
-save(wqglm, file = '../SQI/data/wqglm.RData', compress = 'xz')
-save(habglm, file = '../SQI/data/habglm.RData', compress = 'xz')
 
 # get SQI model results from combined data ----------------------------------
 
-# Not entirely sure what this is doing since it's pulling from the SQI package.
-sqidat2 <- sqidat %>% 
-  rename(
-    CSCI = csci, 
-    ASCI = d_asci
-  ) %>% 
-  sqi %>% 
-  st_as_sf(coords = c('Longitude', 'Latitude'), crs = prj) 
-
 # add predicted pChem and pHab values to sqidat
-sqidat <- sqidat %>%
-  mutate(pChem = predict(wqglm, data.frame(
-      bio_fp = sqidat$bio_fp, 
-      tn = sqidat$tn, 
-      tp = sqidat$tp,
-      Cond = sqidat$Cond)),
-    pHab = predict(habglm, data.frame(
-      hy = sqidat$hy, 
-      pct_safn = sqidat$pct_safn, 
-      xcmg = sqidat$xcmg)))
+sqidat2 <- sqidat %>%
+  mutate(pChem = predict(wqglm, newdata = sqidat, type = "response"),
+    pHab = predict(habglm, newdata = sqidat, type = "response"))
 
 # End of script.
